aee47c809945cc07fde2c9262e02585d657d03ca
elkholy86 <elkholy@windowslive.com>
2020-08-10 15:03:10 +0300

    Data type (22,23)
    in this app we will learn how to make a data type that contains sting and char

---
+++ 22 , 23- Simple Data Type CharStringBoolTypes Part2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _22___23__Simple_Data_Type_CharStringBoolTypes_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char l = 'A';
+            int myvarvalue = 'A';
+            Console.WriteLine("My character = {0} , and the number is {1}", l, myvarvalue);
+            char C = 'a';
+            int myvarvaluec = 'a';
+            Console.WriteLine("My character = {0} , and the number is {1}", C, myvarvaluec);
+            string statment = "This is my character A value ";
+            Console.WriteLine(statment);
+            string statment1 = "This is my character a value";
+            Console.WriteLine(statment1);
+            Console.WriteLine("{0}({1}) and {2} ({3}).", statment, myvarvalue, statment1, myvarvaluec);
+            //int charmin = char.MinValue;
+            //int charmax = char.MaxValue;
+            //char myvar = 'G';
+            //int myvarvalue = 'G';
+            //char myvarvalue2 = Convert.ToChar(myvarvalue);
+            //Console.WriteLine("Min char = {0} max char = {1} my character = {2} My Variable Number = {3} my conversion value is = {4}", charmin, charmax, myvar, myvarvalue, myvarvalue2);
+            //string mystring = "This is my variable";
+            //Console.WriteLine("\nMy sting is {0}" , mystring);
+            //bool mybool = false ;
+            //Console.WriteLine("My boolean value is {0}", mybool);
+            //Console.ReadKey();
+        }
+    }
+}
