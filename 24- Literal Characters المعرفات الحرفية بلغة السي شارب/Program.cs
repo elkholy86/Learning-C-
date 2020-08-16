c0d549e6f98483ae33b3730f53648a4ff7d736c1
elkholy86 <elkholy@windowslive.com>
2020-08-16 15:19:05 +0300

    27,28 - Mathematical Operations
    Learning C# wth my nephew

---
+++ 24- Literal Characters المعرفات الحرفية بلغة السي شارب/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24__Literal_Characters_المعرفات_الحرفية_بلغة_السي_شارب
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 27- Mathematical Operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _27__Mathematical_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 7;
+            int var2 = 3;
+            int temp1 = var1 + var2;
+            int temp2 = var1 - var2;
+            int temp3 = var1 / var2;
+            int temp4 = var1 * var2;
+            int temp5 = var1 % var2;
+            string a = "I am ";
+            string b = "smart";
+            a = a + b;
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}", temp1, temp2, temp3, temp4, temp5, a);
+            //int myvar1 = 15;
+            //int myvar2 = 10;
+            //int temp = myvar1 + myvar2;
+            //int temp2 = myvar1 - myvar2;
+            //int temp3 = myvar1 % myvar2;
+            //int mynegtivar = -10;
+            //int temp4 = +myvar1;
+            //int temp5 = -myvar1;
+            //int temp6 = +mynegtivar;
+            //string mystr1 = "This is wonderful ";
+            //string mystr2 = "How is it?";
+            //mystr1 = mystr2 + mystr1;
+            //Console.WriteLine("{0}\n\n{1}\n\n{2}", temp4, temp5, temp6);
+            //Console.WriteLine("{0}", mystr1);
+            ////Console.WriteLine("The result is {0}, The result 2 is {1} , the remaining is {2}", temp, temp2, temp3);
+            //Console.ReadKey();
+        }
+    }
+}
