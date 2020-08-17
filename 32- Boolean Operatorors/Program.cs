3c082dbfe836016534b75850de366c4a83a592be
elkholy86 <elkholy@windowslive.com>
2020-08-17 15:23:02 +0300

    32- Boolean Operatorors
    Learning c# with my nephew

---
+++ 32- Boolean Operatorors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _32__Boolean_Operatorors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool myresult;
+            int myvar1 = 9 , myvar2 = 11 ;
+            //myresult = myvar1 != 8;
+            myresult = (myvar1 < 10) || (myvar2 <= 11);
+            Console.WriteLine("My result is = {0}" , myresult);
+            Console.ReadKey();
+        }
+    }
+}
