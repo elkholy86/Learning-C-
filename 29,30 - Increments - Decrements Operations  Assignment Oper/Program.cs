2d3ad128c95d6ed2d8a08c81f802d134424b04c5
elkholy86 <elkholy@windowslive.com>
2020-08-17 15:12:29 +0300

    29,30 - Increments - Decrements Operations & Assignment Operations
    Learning c# with my nephew

---
+++ 29,30 - Increments - Decrements Operations  Assignment Oper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _29_30___Increments___Decrements_Operations__Assignment_Oper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int temp,  myvar1 = 7, myvar2 = 2;
+            //myvar1 = myvar2;
+            //myvar1 -= myvar2;
+           temp =  myvar1 /= ++myvar2;
+            Console.WriteLine("The result is = {0}", temp);
+            //***********************************************************
+            //int temp1, myvar1 = 5, myvar2 = 2;
+            //temp1 = myvar1-- / ++myvar2;
+            //Console.WriteLine("{0}", temp1);
+            //Console.ReadKey();
+        }
+    }
+}
