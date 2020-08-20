2be1850cac0f8b86f0971f21bd92d4eeda771a32
elkholy86 <elkholy@windowslive.com>
2020-08-20 09:51:37 +0300

    37 - Ternary Condition
    Learning with C# with Nephew

---
+++ 37 - Ternary Condition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _37___Ternary_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "nazar", result;
+            result = (myname.Equals("Nazar")) ? "yes" : "NO";
+            Console.WriteLine("My variable Result is {0}", result);
+            Console.ReadKey();
+            //************
+            //int myvar = 9;
+            //string myresult = (myvar < 10) ? "Less than 10" : "Greater than 10";
+            //Console.WriteLine("My variable Result is {0}", myresult);
+        }
+    }
+}
