189a4af0ee584b220d60c7e120551a658c0155ef
elkholy86 <elkholy@windowslive.com>
2020-09-27 07:17:46 +0300

    43-Break Loop_Break Command and challenge
    learning C# with my nephew

---
+++ 43-Break Loop_Break Command and challenge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _43_Break_Loop_Break_Command_and_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i > 6)
+                    break;
+                for (int n = 0; n <= 10; n++)
+                {
+                    int y = i * n;
+
+
+                    Console.WriteLine("{0} * {1} = {2}", i, n, y);
+
+                }
+                Console.WriteLine("********************");
+            }
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Nazar");
+            //   if (i > 2)
+            //        break;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello Nazar");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+        }
+    }
+}
