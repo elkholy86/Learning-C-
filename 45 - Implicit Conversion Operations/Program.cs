02f7e36159da83a977a0a1d1364063a548fa6122
elkholy86 <elkholy@windowslive.com>
2020-10-11 01:18:09 +0300

    45- Implicit Conversion Operations
    learning c#

---
+++ 45 - Implicit Conversion Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _45___Implicit_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myvar = 1000;
+            float myvar2 = myvar;
+            Console.WriteLine($"{myvar2}");
+            char myvar3 = 'G'; 
+            int myintvar = myvar3;
+            Console.WriteLine($"{myintvar}");
+            Console.ReadKey();
+
+        }
+    }
+}
