f7ed70ad95e0f1ea1eccad84fdc0557c92954249
elkholy86 <elkholy@windowslive.com>
2020-10-11 01:47:10 +0300

    46- Explicit Conversion and Overflow case
    Learning C#  with my nephew

---
+++ 46- Explicit Conversion and Overflow case/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _46__Explicit_Conversion_and_Overflow_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte mysbyte;
+            int myint = 350;
+           // mysbyte = myint;
+            mysbyte = (byte)myint;
+            Console.WriteLine($"My variable int = {myint}" + "\n");
+            Console.WriteLine($"My Sbyte variable = {mysbyte}" + "\n");
+            Console.ReadKey();
+        }
+    }
+}
