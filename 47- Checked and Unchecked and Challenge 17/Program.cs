7fb8151bdc0f91b6070451d887c5c825e158d1c2
elkholy86 <elkholy@windowslive.com>
2020-10-12 01:48:20 +0300

    47- Checked and Unchecked and Challenge 17
    learning c# with my nephew

---
+++ 47- Checked and Unchecked and Challenge 17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _47__Checked_and_Unchecked_and_Challenge_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myint;
+            decimal Elkholy = 1000;
+            myint = checked((int)Elkholy);
+            Console.WriteLine($"My variable int = {myint}" + "\n");
+            //*************************
+            //byte mysbyte;
+            //int myint = 350;
+            //mysbyte = unchecked((byte)myint);
+            //Console.WriteLine($"My variable int = {myint}" + "\n");
+            //Console.WriteLine($"My byte variable = {mysbyte}");
+            //Console.ReadKey();
+        }
+    }
+}
