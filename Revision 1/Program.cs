4913fe2895a958e4a5a0e0e5f022413a9fdadb12
elkholy86 <elkholy@windowslive.com>
2020-10-20 02:25:31 +0300

    Revision 1
    Learning C# with my nephew

---
+++ Revision 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Revision_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //24 - Literal Characters
+            int mvar = -100;
+            uint myuvar = 1000u; 
+            float myflvar = 1.5F;
+            double mydvar = 100.5;
+            decimal mydecvar = 100.5m;
+            Console.WriteLine("{0}{1}",mvar,myuvar);
+            Console.WriteLine($"{mydecvar},{ mydvar}");
+            // 22,23 - Simple Data Type CharStringBoolTypes
+            //char myvar = 'A';
+            //int myvarvalue = 'A';
+
+            //char myvar1 = 'a';
+            //int myvarvalue1 = 'a';
+
+            //string mystring = "This is My Character (A) value";
+            //string mystring1 = "This is My Character (a) value";
+            //Console.WriteLine($"{mystring} , My character = {myvar} and My variable Number = {myvarvalue} ");
+            //Console.WriteLine($"{mystring1} , My character = {myvar1} and My variable Number = {myvarvalue1} ");
+            //Console.WriteLine($"{mystring} = {myvarvalue} ,and {mystring1} = {myvarvalue1} ");
+
+            //int charmin = char.MinValue;
+            //int charmax = char.MaxValue;
+            //char myvar = 'G';
+            //int myvarvalue = 'G';
+            //char myvarvalue2 = Convert.ToChar(75);
+            //Console.WriteLine("Min char = {0} max char = {1} My character = {2} My variable Number = {3} My converion value is = {4}", charmin, charmax, myvar, myvarvalue, myvarvalue2);
+            //string mystring = "This is My varible";
+            //Console.WriteLine("\n my string is {0}", mystring);
+            //bool mybool = true;
+            //bool mybool1 = false;
+            //Console.WriteLine("My Boolean value is {0}, My Second Boolean value is {1}", mybool,mybool1);
+            //Console.ReadKey();
+            //*****************************//
+            //21 - Simple DataType - FloatPointType
+            //float flmin = float.MinValue;
+            //float flmax = float.MaxValue;
+            //Console.WriteLine("Min value = {0},Max val = {1}",flmin,flmax);
+            //float flvar = 1f / 3;
+            //double dlvar = 1d / 3;
+            //Decimal dcvar = 1m / 3;
+            //Console.WriteLine("\nfloat value = {0}\nDouble value = {1}\nDecimal value = {2}  ", flvar, dlvar, dcvar);
+            //Console.ReadKey();
+            //*****************************//
+            // 12 first programme 
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+        }
+    }
+}
