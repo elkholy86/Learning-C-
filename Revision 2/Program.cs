1780aaad10ce6ab3cb2f55792fc09b1f320cb47c
elkholy86 <elkholy@windowslive.com>
2020-10-21 01:16:30 +0300

    Revion 2
    24 lesson

---
+++ Revision 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Revision_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = "Nazar has said This is a unique course ,and i think so";
+            string mystring2 = @"Yes it's";
+            string mystring3 = mystring + mystring2;
+            Console.WriteLine("{0}", mystring);
+            Console.ReadKey();
+            //string mystring = "Nazar said This is a good course ,and i think";
+            //string mystring2 = @"Yes it's";
+            //string mystring3 = mystring + mystring2;
+            //Console.WriteLine("{0}",mystring);
+            //Console.ReadKey();
+        }
+    }
+}
