06362aa5a3d58d8e4a5cb2304c9ddba06e56b3fb
elkholy86 <elkholy@windowslive.com>
2020-10-24 02:05:53 +0300

    27- Mathematical Operations

--- Revision 2/Program.cs
+++ Revision 2/Program.cs
@@ -6,11 +6,44 @@ namespace Revision_2
     {
         static void Main(string[] args)
         {
-            string mystring = "Nazar has said This is a unique course ,and i think so";
-            string mystring2 = @"Yes it's";
-            string mystring3 = mystring + mystring2;
-            Console.WriteLine("{0}", mystring);
+            // 27 & 28 - Mathematical Operations
+            int var1 = 7;
+            int var2 = 3;
+            int temp1 = var1 + var2;
+            int temp2 = var1 - var2;
+            int temp3 = var1 / var2;
+            int temp4 = var1 * var2;
+            int temp5 = var1 % var2;
+            string a = "I am ";
+            string b = "smart";
+            a = a + b;
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}", temp1, temp2, temp3, temp4, temp5, a);
+            //int myvar1 = 15;
+            //int myvar2 = 10;          
+            //int temp = myvar1 + myvar2;
+            //int temp2 = myvar1 - myvar2;
+            //int temp3 = myvar1 % myvar2;
+            //string mysir = "This is wonderful ";
+            //string mysir2 = " How is it";
+            //mysir = mysir + mysir2;
+            //Console.WriteLine("the result {0} the result2 is {1} The remaining is {2}", temp, temp2, temp3);
+            //Console.WriteLine("{0}" ,mysir );
+            //int mynegativevar = -10;
+            //int temp4 = +myvar1;
+            //int temp5 = -myvar1;
+            //int temp6 = +mynegativevar;
+            //Console.WriteLine("{0} \n\n {1}\n\n  {2}", temp4, temp5,temp6);
+            //
+            //int temp = myvar1 + myvar2;
+            //int temp2 = myvar1 - myvar2;
+            //int temp3 = myvar1 % myvar2;
+            //Console.WriteLine("The result is {0} The result2 is {1} The remaining is {2}",temp,temp2,temp3);
             Console.ReadKey();
+            //string mystring = "Nazar has said This is a unique course ,and i think so";
+            //string mystring2 = @"Yes it's";
+            //string mystring3 = mystring + mystring2;
+            //Console.WriteLine("{0}", mystring);
+            //Console.ReadKey();
             //string mystring = "Nazar said This is a good course ,and i think";
             //string mystring2 = @"Yes it's";
             //string mystring3 = mystring + mystring2;
