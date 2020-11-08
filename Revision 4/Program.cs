cdc7eb79d28cccf6d21b9d533e9dc7f0ee2f92d8
elkholy86 <elkholy@windowslive.com>
2020-11-09 00:46:26 +0300

    revision 4
    learning C# with my nephew

--- Revision 4/Program.cs
+++ Revision 4/Program.cs
@@ -6,25 +6,81 @@ namespace Revision_4
     {
         static void Main(string[] args)
         {
-            //34 , 35  - Nested If Else Statement
-            int myvar = 4, myvar2 = 6;
-            if (myvar <= 3 && myvar2 >= 5)
-            {
-                Console.WriteLine("Condtition one right");
-            }
-            else if (myvar <= 4 && myvar2 >= 6)
-            {
-                Console.WriteLine("Condition two is right");
-            }
-            else if (myvar2 > 5)
-            {
-                Console.WriteLine("Condition three is right");
-            }
-            else
-            {
-                Console.WriteLine("no one of the conditions");
-            }
+            //37 - Ternary Condition
+            string myname = "nazar", result;
+            result = (myname.Equals("Nazfar")) ? "Yes" : "No";
+            Console.WriteLine("My variable result is {0}", result);
             Console.ReadKey();
+            //int myvar = 9;
+            //string myresult = (myvar < 8) ? "Less than 10" : "greater than 10";
+            //Console.WriteLine("My variable result is {0}" , myresult);
+                        // 36 - Switch Statement, Challenge
+            //string whathesay = "Hello";
+            //switch (whathesay)
+            //{
+            //    case "Bye":
+            //        Console.WriteLine("Bye");
+            //         break;
+            //    case "Hello":
+            //        Console.WriteLine("Hello");
+            //        break;
+            //    case "Welcome":
+            //        Console.WriteLine("Welcome");
+            //        break;
+            //    default:
+            //        break;
+            //}
+            //int myvar = 15;
+            //switch (myvar)
+            //{
+            //    case 10:
+            //        Console.WriteLine("My variable number value is 10");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("My variable number value 7");
+            //        break;
+            //    case 15:
+            //        Console.WriteLine("My variable number value is 15");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+            //string myanimal = "Cat";
+            //switch (myanimal)
+            //{
+            //    case "canary":
+            //        Console.WriteLine("My animal is canary bird");
+            //        break;
+            //    case "Cat":
+            //        Console.WriteLine("My animal is a cat");
+            //        break;
+            //    case "camel":
+            //        Console.WriteLine("My animal is a camel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break; 
+            //}
+            //34 , 35  - Nested If Else Statement
+            //int myvar = 4, myvar2 = 6;
+            //if (myvar <= 3 && myvar2 >= 5)
+            //{
+            //    Console.WriteLine("Condtition one right");
+            //}
+            //else if (myvar <= 4 && myvar2 >= 6)
+            //{
+            //    Console.WriteLine("Condition two is right");
+            //}
+            //else if (myvar2 > 5)
+            //{
+            //    Console.WriteLine("Condition three is right");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no one of the conditions");
+            //}
+            //Console.ReadKey();
 
             //*********
             //int myvar1 = 10 , myvar2 = 6 , myvar3 = 8 ;
