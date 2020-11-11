b5e96a4c8dfd5d8a876b635a896612175903e05a
elkholy86 <elkholy@windowslive.com>
2020-11-12 01:31:10 +0300

    Revision 5
    Learning C#

---
+++ Revision 5/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Revision_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       //     42 - Nested Loop and Challenge
+            // //int n = 1;
+            //for (int i = 0; i <= 6; i+=2)
+            //{
+            //    for (int n = 0; n <= 12; n++)
+            //    {
+            //        int y = i * n;
+            //        Console.WriteLine("{0} * {1} = {2}", i, n, y);
+            //    }
+            //}
+//**************************************
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Hello Nazar");
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Console.WriteLine("\t Hello Nazar");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+            //Console.ReadKey();
+
+
+
+            //*******************************************************
+            //double s = 20, total = 1000;
+            //int  i = 0;
+            //for ( i = 0; total < 2000; i++)
+            //{
+            //    total += s;
+            //}
+            //Console.WriteLine($"the years that we want to reach 2000 is {i} month");
+            //******************************
+            //double exlose = 1, total = 120;
+            //for (int i = 0; i < 83; i++)
+            //{
+            //    total -= exlose;
+            //}
+            //Console.WriteLine("The Exercises that we need to reach 83KG is {0}", total);
+            //*****************
+            //int mysalary = 1000 , total = 0;
+            //   for (int i = 0; i < 12; i++)
+            //   {
+            //       total += mysalary;
+            //   }
+            //   Console.WriteLine("My salary for 12 months is {0}", total);
+            //****************************
+            //double exlose = 1, total = 120;
+            // int i = 0;
+            //while (i < 83)
+            //{
+            //    i++;
+            //    total -= exlose;
+            //    //Console.WriteLine("i {0}", i);
+            //}
+            //Console.WriteLine("The Exercises that we need to reach 83KG is {0}", total);
+            //**********************
+            //int i = 0; 
+            //double mysalary = 1020.30 , total = 0.0;
+            //while (i < 15)
+            //{
+            //    total += mysalary;
+            //    i++; 
+            //}
+            //Console.WriteLine("My salary is {0}",total);
+            //Console.ReadKey();
+            //*****************
+            //double s = 20, total =1000;
+            //double i = 0;
+            //do
+            //{
+            //    total += s;
+            //    i++;
+            //    Console.WriteLine($"{i},{total}");
+            //} while (total  < 2000);
+            //double y = 0;
+            //y = i / 12;
+            //Console.WriteLine($"the years that we want to reach 2000 is {i} month , and the years is {y}");
+
+            // console.writeline("salary values equal {0}", total);
+            //int i = 0;
+            //do
+            //{
+            //    total += mysalary;
+            //    i++;
+            //    Console.WriteLine("I values equal {0}", i);
+            //} while (i < 10);
+            //Console.WriteLine("Salary values equal {0}", total);
+        }
+    }
+}
