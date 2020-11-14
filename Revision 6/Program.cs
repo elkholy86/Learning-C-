a1294aebe97b8c18d8c562279dacbe7358b06859
elkholy86 <elkholy@windowslive.com>
2020-11-15 00:44:59 +0300

    Revision 6 45 - Implicit Conversion Operations 44 - Break Loop _ Continue Command and challenge 1643 - Break Loop_Break Command and challenge 15
    45 - Implicit Conversion Operations

---
+++ Revision 6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Revision_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //**********************************
+            //45 - Implicit Conversion Operations
+            //               long myvar = 1000;
+            //   float myvar2 = myvar;
+            ////   Console.WriteLine($"{myvar2}");
+            //   char myvar3 = 'G';
+            //   int myintvar = myvar3;
+            //   Console.WriteLine($"{myintvar}");
+            //   Console.ReadKey();
+            //******************************
+            //44 - Break Loop _ Continue Command and challenge 16
+            //for (int i = 0; i <= 10; i++)
+            //{
+
+            //    for (int n = 0; n <= 12; n++)
+            //    {
+            //        if (n ==7)
+            //            continue;
+            //        int y = i * n;
+            //        Console.WriteLine("{0} * {1} = {2}", i, n, y);
+            //    }
+            //    Console.WriteLine("-----------------------");
+            //}
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    if (i > 2)
+            //        continue;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("Hello World");
+            //    }
+            //    Console.WriteLine("Hello Nazar");
+            //    Console.WriteLine("\n");
+            //}
+            //Console.ReadKey();
+            //************************
+            //for (int i = 0; i <= 5 ; i++)
+            //{
+            //    Console.WriteLine("Hello Nazar");
+            //    if (i > 2)
+            //        continue;
+            //    for (int j = 0;  j<= 4 ; j++)
+            //    {
+            //        Console.WriteLine("Hello World");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+            //Console.ReadKey();
+
+            //43 - Break Loop_Break Command and challenge 15
+            //for (int i = 0; i <= 10; i ++)
+            //{
+
+            //    for (int n = 0; n <= 12; n++)
+            //    {
+            //        if (n == 8)
+            //            break;
+            //        int y = i * n;
+            //        Console.WriteLine("{0} * {1} = {2}", i, n, y);
+            //    }
+            //    Console.WriteLine("-----------------------");
+            //}
+            //***********************
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Nazar");
+            //    //if (i > 2)
+            //    //    break;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello World");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
