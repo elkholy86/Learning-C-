c7087340642e1becec83d2b500bd03f301025b8d
elkholy86 <elkholy@windowslive.com>
2020-11-22 09:20:42 +0300

    53- Array and ForLoop and Challenge
    Learning C#

---
+++ 53- Array and ForLoop and Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _53__Array_and_ForLoop_and_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int s = 5;
+            int[] arr = new int[s] { 90, 20, 10, 9, 100 };
+            int max = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            Console.WriteLine($"The max value in this array = {max} ");
+          //  Console.ReadKey();
+            //*************************
+            const int myarraysize = 5;
+            int[] mynumbers = { 1, 5, 8, 9, 7 };
+            int[] mynumbers2 = new int[myarraysize];
+            int[] mynumbers3 = new int[myarraysize] { 1, 5, 8, 9, 7 };
+            //            Console.WriteLine($"{mynumbers[3]}");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"my num {i + 1} = {mynumbers[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
