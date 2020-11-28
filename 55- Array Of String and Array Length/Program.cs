7b9755b8953e458cc66995f65922cdb9235ea37f
elkholy86 <elkholy@windowslive.com>
2020-11-28 09:06:39 +0300

    55- Array Of String and Array Length
    55- Array Of String and Array Length and revision for lesson 12

---
+++ 55- Array Of String and Array Length/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _55__Array_Of_String_and_Array_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //**** 55- Array Of String and Array Length *******
+            //string[] myfullname = { "Hossam" , "Saad" , "EL-kholy"};
+            //int myarraylenght = myfullname.Length;
+            //for (int i = 0; i < myarraylenght; i++)
+            //{
+            //    Console.Write($"{myfullname[i]} " );
+            //}
+            //Console.WriteLine("\nMy array lenght is " + $"{myarraylenght}");
+            //******** (Revision of lesson 12) ********
+            Console.WriteLine("Hello world");
+           Console.ReadKey();
+            Console.WriteLine("Hello live");
+        }
+    }
+}
