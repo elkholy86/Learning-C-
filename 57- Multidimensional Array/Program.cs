a420c5a76c37172a53966e9b5213679c7e10ac0a
elkholy86 <elkholy@windowslive.com>
2020-12-04 08:17:27 +0300

    57 - Multidimensional Array
    learning c# with my nephew

---
+++ 57- Multidimensional Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _57__Multidimensional_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //***** 57 - Multidimensional Array ************
+            //int[,] mynumbers = {
+            //    { 1 , 2 , 3 , 4} ,
+            //    { 5 , 6 , 7 , 8} ,
+            //    { 9 , 10 , 11 , 12}
+            //  };
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        Console.WriteLine("{0}", mynumbers[i, j] + " ");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            //foreach (int item in mynumbers)
+            //{
+            //    Console.WriteLine("{0}" , item + " ");
+            //}
+
+            //int[,,] myarray3da = new int[2, 2, 3]
+            //    {
+            // { 
+            //    { 1 , 2 , 3 } , { 4 , 5 , 6}
+            //    },
+            //{
+            //    { 7 , 8 , 9} , { 10 , 11 , 12}
+            //}                
+            //    };
+
+            // for (int i = 0; i < 2; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+
+            //        for (int k = 0; k  < 3 ; k++)
+            //        {
+            //            Console.Write( myarray3da[i, j , k] + " ");
+            //        }
+            //    }
+            //Console.WriteLine(" \n ");
+            //}
+        }
+    }
+}
