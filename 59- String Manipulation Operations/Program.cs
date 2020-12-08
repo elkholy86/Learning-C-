2404c0c3945222e767a3d4ca09d74cf24a8274b7
elkholy86 <elkholy@windowslive.com>
2020-12-08 07:57:11 +0300

    59- String Manipulation Operations and Challenge
    59- String Manipulation Operations and Challenge

---
+++ 59- String Manipulation Operations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _59__String_Manipulation_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string mystring = "I am smart", subsrting = "He's";
+            //char[] mychararray = mystring.ToCharArray();
+            //for (int i = 0; i < subsrting.Length; i++)
+            //{
+            //    mychararray[i] = subsrting[i];
+            //}
+            //foreach (char cc in mychararray)
+            //{
+            //    Console.Write(cc);
+            //}
+            //Console.ReadKey();
+            //*********
+
+            //string mystring = "Hello My friends";
+            //char[] mychararray = mystring.ToCharArray();
+            //Console.WriteLine($"The String lenght is {mystring.Length}");
+            //foreach (char mychar in mychararray)
+            //{
+            //    Console.WriteLine($"{mychar}");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
