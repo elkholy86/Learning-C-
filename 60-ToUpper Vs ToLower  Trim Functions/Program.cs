b2c4bcd253ff4ff180e41e6602a30fff4bb9a2e9
elkholy86 <elkholy@windowslive.com>
2020-12-08 08:29:11 +0300

    60-ToUpper Vs ToLower & Trim Functions
    60-ToUpper Vs ToLower & Trim Functions

---
+++ 60-ToUpper Vs ToLower  Trim Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _60_ToUpper_Vs_ToLower__Trim_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring, mystring2;
+            // Console.WriteLine("Please Enter your string");
+            // mystring = Console.ReadLine();
+            char[] mytrimchar = { 'h' , 'd' , 'i' , ' '};
+            Console.WriteLine("Please Enter your string");
+            mystring = Console.ReadLine();
+            mystring = mystring.ToLower();
+            mystring2 = mystring.Trim(mytrimchar);
+            // mystring2 = mystring.ToUpper();
+            //Console.WriteLine($"{mystring2}");
+            //mystring2 = mystring.ToLower();
+            // Console.WriteLine($"{mystring2}");
+           // mystring2 = mystring.Trim();
+            Console.WriteLine($"{mystring2}");
+            Console.ReadKey();
+        }
+    }
+}
