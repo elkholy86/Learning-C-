2d2f4f68066812e998319486164603a6791e8794
elkholy86 <elkholy@windowslive.com>
2020-12-10 08:07:49 +0300

    62- Split Function with String Type
    62- Split Function with String Type

---
+++ 62- Split Function with String Type/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _62__Split_Function_with_String_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ms;
+            char[] sep = { ' '};
+            ms = Console.ReadLine();
+            string [] mynewwords;
+            mynewwords = ms.Split(sep);
+            foreach (string  word in mynewwords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
