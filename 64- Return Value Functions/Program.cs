0c67d26f8ee81dd731a6f552f5339cc2ba706cd8
elkholy86 <elkholy@windowslive.com>
2020-12-12 07:58:05 +0300

    64- Return Value Functions
    64- Return Value Functions

---
+++ 64- Return Value Functions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _64__Return_Value_Functions
+{
+    class Program
+    {
+
+        static void addnumbers(int myfirstnumbr, int mysecondnumber)
+        {
+            int temp = myfirstnumbr + mysecondnumber;
+            Console.WriteLine($"{temp}");
+        }
+        static int addnumbers2(int myfirstnumbr, int mysecondnumber)
+        {
+            int temp = myfirstnumbr + mysecondnumber;
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            int myvar1 = 5, myvar2 = 3 , temp;
+         addnumbers(myvar1 , myvar2);
+           // temp = addnumbers2(myvar1, myvar2);
+            Console.WriteLine($"{addnumbers2(myvar1, myvar2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
