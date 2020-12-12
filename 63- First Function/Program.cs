3848502f2bd7f84f4543846a7f0e5dc8e9b50335
elkholy86 <elkholy@windowslive.com>
2020-12-12 07:32:57 +0300

    63- First Function
    63- First Function

---
+++ 63- First Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _63__First_Function
+{
+    class Program
+    {
+        static void printname()
+        {
+            Console.WriteLine("my name is mohammed");
+        }
+
+        static void Main(string[] args)
+        {
+            printname();
+        }
+    }
+}
