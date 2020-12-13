5e90dc68ddb25456f30f7738d76945a2e54087e9
elkholy86 <elkholy@windowslive.com>
2020-12-13 08:01:06 +0300

    66- Reference values and challenge
    66- Reference values and challenge

---
+++ 66- Reference values and challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _66__Reference_values_and_challenge
+{
+    class Program
+    {
+        static void Mul (ref int fs , ref int sec )
+        {
+            fs*= 4;
+            sec *= 4;
+            Console.WriteLine($"the result is = {fs} , {sec}");
+        }
+
+        //static void multiplynum(ref int myvar)
+        //{
+        //    myvar *= 3;
+        //    Console.WriteLine($"My Number = {myvar}");
+        //}
+        static void Main(string[] args)
+        {
+            int fnumber = 9 , snum = 10; 
+            Console.WriteLine($"the numbers is = {fnumber} , {snum}");
+            Mul (ref fnumber , ref snum);
+            Console.WriteLine($"the numbers is = {fnumber} , {snum}");
+        }
+    }
+}
