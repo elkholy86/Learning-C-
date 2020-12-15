7d0cb52ef87cb79dcb273bbe5ed8e0179d30eb08
elkholy86 <elkholy@windowslive.com>
2020-12-15 08:09:49 +0300

    68 - Variables Scope
    68 - Variables Scope

---
+++ 68- Variables Scope/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _68__Variables_Scope
+{
+    class Program
+    {
+        static string mystring2;
+        static void printmystring()
+        {
+            string mystring = "My String inside the printmystring method";
+            Console.WriteLine($"{mystring}");
+            Console.WriteLine($"{mystring2}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            // 68 - Variables Scope
+            string mystring = "My string inside the main method";
+            printmystring();
+            Console.WriteLine($"{mystring}");
+            Console.WriteLine($"{mystring2}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
