54c12d87f9bf46ce24a4b553c1681ce2ca64a150
elkholy86 <elkholy@windowslive.com>
2020-12-15 08:26:22 +0300

    69- Variable Scope in Structures
    69- Variable Scope in Structures

---
+++ 69- Variable Scope in Structures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _69__Variable_Scope_in_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //** 69- Variable Scope in Structures
+            int i, index = 0;
+            for (i = 0; i < 10; i++)
+            {
+                index = i;
+                Console.WriteLine($"{index}");
+            }
+            Console.WriteLine($"last index output in loop: {index}");
+            Console.ReadKey();
+        }
+    }
+}
