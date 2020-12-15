e6cdfb3690d0c8b75a133048eb74bc6800ca3f92
elkholy86 <elkholy@windowslive.com>
2020-12-15 08:49:21 +0300

    70- The Main Method
    70- The Main Method

---
+++ 70- The Main Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _70__The_Main_Method
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // 70- The Main Method
+         Console.WriteLine($"{args.Length} Command Line arguments has entered");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
