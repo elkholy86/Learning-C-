c26c5245c4cd3c0bf7a58f5cc3a766be1b5cb3e5
elkholy86 <elkholy@windowslive.com>
2020-12-17 07:42:44 +0300

    71- Struct Function
     71- Struct Function

---
+++ 71- Struct Function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _71__Struct_Function
+{
+    class Program
+    {
+        struct fullname 
+        {
+            
+            
+            public string myfirstname, mylastname;
+            public string printname ()=> myfirstname + " " + mylastname;
+        }
+        static void Main(string[] args)
+        {
+            // 71- Struct Function 
+            fullname myfullname;
+            myfullname.myfirstname = "Hossam";
+            myfullname.mylastname = "Elkholy";
+            //            Console.Write($"{myfullname.myfirstname} {myfullname.mylastname}");
+            Console.Write(myfullname.printname());
+            Console.ReadKey();
+
+        }
+    }
+}
