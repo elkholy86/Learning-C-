90824e74f6f1340eab42f0c1c6768d66807e82f9
elkholy86 <elkholy@windowslive.com>
2020-12-19 08:21:50 +0300

    73- Delegate Type
    73- Delegate Type

---
+++ 73- Delegate Type/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _73__Delegate_Type
+{
+    //*** 73- Delegate Type  *************
+
+    delegate int mynewnumber(int mynumber);
+        class Program
+    {
+        static int myxnumber = 3;
+        static int addfunct(int number)
+        {
+            myxnumber += number;
+            return myxnumber;
+        }
+
+        static int mulfunc (int number)
+        {
+            myxnumber *= number;
+            return myxnumber;
+        }
+        static void Main(string[] args)
+        {
+            mynewnumber n1 = new mynewnumber(addfunct);
+            mynewnumber n2 = new mynewnumber(mulfunc);
+            n1(4);
+            Console.WriteLine($"{myxnumber}");
+            n2(5);
+            Console.WriteLine($"{myxnumber}");
+            Console.ReadKey();
+        }
+    }
+}
