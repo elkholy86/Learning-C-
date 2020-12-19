30846b7b00abf586feb46f51b01299abe8a10e5b
elkholy86 <elkholy@windowslive.com>
2020-12-19 08:48:55 +0300

    73 - Delegate Type
    22,23 - Simple Data Type CharStringBoolTypes

--- 73- Delegate Type/Program.cs
+++ 73- Delegate Type/Program.cs
@@ -4,30 +4,49 @@ namespace _73__Delegate_Type
 {
     //*** 73- Delegate Type  *************
 
-    delegate int mynewnumber(int mynumber);
+  //  delegate int mynewnumber(int mynumber);
         class Program
     {
-        static int myxnumber = 3;
-        static int addfunct(int number)
-        {
-            myxnumber += number;
-            return myxnumber;
-        }
+        //static int myxnumber = 3;
+        //static int addfunct(int number)
+        //{
+        //    myxnumber += number;
+        //    return myxnumber;
+        //}
 
-        static int mulfunc (int number)
-        {
-            myxnumber *= number;
-            return myxnumber;
-        }
+        //static int mulfunc (int number)
+        //{
+        //    myxnumber *= number;
+        //    return myxnumber;
+        //}
         static void Main(string[] args)
         {
-            mynewnumber n1 = new mynewnumber(addfunct);
-            mynewnumber n2 = new mynewnumber(mulfunc);
-            n1(4);
-            Console.WriteLine($"{myxnumber}");
-            n2(5);
-            Console.WriteLine($"{myxnumber}");
-            Console.ReadKey();
+            // ***** 22,23 - Simple Data Type CharStringBoolTypes *****
+            int nv = 'A', nv2 = 'a';
+            string chv = "This is my character (A) value =";
+            string chv2 = "This is my character (a) value =";
+            Console.WriteLine($"{chv} {nv} and {chv2} {nv2}");
+
+            // ***** 22,23 - Simple Data Type CharStringBoolTypes *****
+            //int charmin = Char.MinValue;
+            //int charmax = Char.MaxValue;
+            //char myvar = 'G';
+            //int myvarvalue = 'G';
+            //char myvarvalue2 = Convert.ToChar(myvarvalue);
+            //string mystring = "This is my variable";
+            //Console.WriteLine($"my string is {mystring}");
+            //bool mybool = false;
+            //Console.WriteLine($"my boolean value is {mybool}");
+            //Console.WriteLine($"Min char = {charmin} , max char = {charmax} , my character = {myvar} , my variable number = {myvarvalue} , my conversion value is {myvarvalue2}");
+            //Console.ReadKey();
+            //*** 73- Delegate Type  *************
+            //mynewnumber n1 = new mynewnumber(addfunct);
+            //mynewnumber n2 = new mynewnumber(mulfunc);
+            //n1(4);
+            //Console.WriteLine($"{myxnumber}");
+            //n2(5);
+            //Console.WriteLine($"{myxnumber}");
+            //Console.ReadKey();
         }
     }
 }
---
+++ 74- Debugging Mod Vs Release Mod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _74__Debugging_Mod_Vs_Release_Mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
