6111f31d6e0709121dbdf218b8572c4bcd578dfe
elkholy86 <elkholy@windowslive.com>
2020-12-20 08:37:30 +0300

    75- BreakPoints
    75- BreakPoints

--- 75- BreakPoints/Program.cs
+++ 75- BreakPoints/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
+using static System.Console;
 
 namespace _75__BreakPoints
 {
     class Program
     {
+        // 75- BreakPoints
+        public static int addfunctions(int x, int y)
+        {
+            Debug.WriteLine("We started here in the add funtion");
+            int z = x + y;
+            return z;
+        }
+        public static int mulfunctions(int x, int y)
+        {
+            Debug.WriteLine("We started here in the mult funtion");
+            int z = x * y;
+            Debug.WriteLine("We are done here in the add funtion");
+            Trace.WriteLine("We are done here in the add funtion");
+            return z;
+        }
+
+        public static void writefunction(int z)
+        {
+            Debug.WriteLine("We started here in the write funtion");
+            WriteLine($"{z}");
+            Debug.WriteLine("We done here in the write funtion");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int mynum, myvar1 = 4, myvar2 = 7;
+            mynum = addfunctions(myvar1, myvar2);
+            writefunction(mynum);
+            mynum = mulfunctions(myvar1, myvar2);
+            writefunction(mynum);
+            ReadKey();
         }
     }
 }
+
