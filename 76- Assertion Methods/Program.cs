2ed35d83439f48da484d5db0e5d65abe627836c7
elkholy86 <elkholy@windowslive.com>
2020-12-22 07:23:30 +0300

    76 - Assertion Methods
    76 - Assertion Methods

---
+++ 76- Assertion Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+
+namespace _76__Assertion_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *** 76 - Assertion Methods ***
+            WriteLine($"Enter A value");
+            int i = Convert.ToInt32(ReadLine());
+            if (i >= 100)
+            {
+                WriteLine($"{i}");
+            }
+            else
+            {
+                Debug.WriteLine("We are here");
+                Debug.Assert(i >= 100 , "short" , "long");
+                Trace.Assert(i >= 100, "short", "long");
+            }
+            WriteLine("Done");
+            ReadKey();
+        }
+    }
+}
