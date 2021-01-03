210147990f4ac526b8ca8313b556001ef7f7def9
elkholy86 <elkholy@windowslive.com>
2021-01-04 00:56:45 +0300

    82- Multicasting Delegate
    82- Multicasting Delegate

---
+++ 77- Variables Values Monitoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _77__Variables_Values_Monitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 82- Multicasting Delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _82__Multicasting_Delegate
+{
+    delegate int mynewnumber(int mynumber);
+        class Program
+    {
+        static int myxnumber = 3;
+        static int addfunct(int number)
+        {
+            myxnumber += number;
+            return myxnumber;
+        }
+        static int mulfunc(int number)
+        {
+            myxnumber *= number;
+            return myxnumber;
+        }
+        static void Main(string[] args)
+        {
+            //** 82- Multicasting Delegate **** 
+            mynewnumber n;
+            mynewnumber n1 = new mynewnumber(addfunct);
+            mynewnumber n2 = new mynewnumber(mulfunc);
+            n1(4);
+            Console.Write($"{myxnumber}\n");
+            n2(5);
+            Console.Write($"{myxnumber}\n");
+            n = n1 + n2;
+            n(5);
+            Console.Write($"{myxnumber}\n");
+            Console.ReadKey();
+        }
+    }
+}
