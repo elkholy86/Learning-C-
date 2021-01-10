0a351b461fe95ceddb3614bef452b585949fce8a
elkholy86 <elkholy@windowslive.com>
2021-01-11 00:32:45 +0300

    86 - Nullable Types
    86 - Nullable Types

---
+++ 86- Nullable Types/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _86__Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //** 86 - Nullable Types
+            string myvariable = null;
+            int? mynumber = null;
+            Console.WriteLine($"{myvariable}");
+            Console.WriteLine($"{mynumber}");
+            Console.ReadKey();
+        }
+        }
+    }
