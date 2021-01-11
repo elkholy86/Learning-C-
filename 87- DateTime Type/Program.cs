cd9b81b0e2542ca198b00b99b8016c1f653227b3
elkholy86 <elkholy@windowslive.com>
2021-01-11 07:57:12 +0300

    87 - DateTime Type
    87 - DateTime Type

---
+++ 87- DateTime Type/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+using System.Threading.Tasks;
+
+namespace _87__DateTime_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 87 - DateTime Type
+            //WriteLine(DateTime.Now.ToString("hh:mm:ss \nMM dd yyyy"));
+            //DateTime time = DateTime.Now;
+            //string format = "MM dd yyyy h:mm";
+            //WriteLine(time.ToString(format));
+            //DateTime time = DateTime.Now;
+            //string format = "MM ddd d hh:mm:ss yyyy";
+            //WriteLine(time.ToString(format));
+            //  DateTime now = DateTime.Now;
+            //WriteLine(now.ToString("d"));
+            //WriteLine(now.ToString("D"));
+            //WriteLine(now.ToString("f"));
+            //WriteLine(now.ToString("F"));
+            //WriteLine(now.ToString("g"));
+            //WriteLine(now.ToString("G"));
+            //WriteLine(now.ToString("m"));
+            //WriteLine(now.ToString("M"));
+            //WriteLine(now.ToString("o"));
+            //WriteLine(now.ToString("O"));
+            //WriteLine(now.ToString("s"));
+            //WriteLine(now.ToString("t"));
+            //WriteLine(now.ToString("T"));
+            //WriteLine(now.ToString("u"));
+            //WriteLine(now.ToString("U"));
+            //WriteLine(now.ToString("y"));
+            //WriteLine(now.ToString("Y"));
+            //string result = now.ToString("s ");
+            // WriteLine($"{now} [s] = {result}");
+            DateTime now = DateTime.Today;
+            for (int i = 0; i < 7; i++)
+            {
+                WriteLine(now.ToString("dddd"));
+                now = now.AddDays(1);
+            }
+        }
+    }
+}
