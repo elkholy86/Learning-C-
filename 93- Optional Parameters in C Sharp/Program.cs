19b06ee5e57305d43a8da859c2a40b4266cef00f
elkholy86 <elkholy@windowslive.com>
2021-01-24 00:32:18 +0300

    93- Optional Parameters in C Sharp
    93- Optional Parameters in C Sharp

---
+++ 93- Optional Parameters in C Sharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _93__Optional_Parameters_in_C_Sharp
+{
+    class Program
+    {
+        public static void printvalues(string name = "Nazae" , int myvalue = 7)
+        {
+            Console.WriteLine($"name = {name} number = {myvalue}");
+        }
+        static void Main(string[] args)
+        {
+            // 93- Optional Parameters in C Sharp
+            string name = "Ahmed";
+            int value = 15;
+            printvalues();
+            printvalues(name , value);
+            printvalues(name : "basem" , myvalue : 6);
+        }
+    }
+}
