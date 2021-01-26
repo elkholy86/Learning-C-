0abd96414d6c1ac465739c45851a4b3fe8895cbe
elkholy86 <elkholy@windowslive.com>
2021-01-26 08:34:52 +0300

    Revision on 24 , 25 , 26
    Revision on 24 , 25 , 26

---
+++ Revision on 24 , 25 , 26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Revision_on_24___25___26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 25 - String Type and Escape Characters
+          // string mystring = "\fHossam Elkholy Said  This is a unique course, and i think so";
+            //string mystring = "Elkholy hossam \' This is args good cours\', and i think";
+            //string mystring = "Elkholy hossam \\ This is args good cours\\, and i think";
+            //string mystring = "Elkholy hossam \\ This is args good cours\\, and i think\a";
+            //            string mystring = "Elkholy hossam  This\r is args good cours, and i think";
+            //string mystring1 = "Welcome back";
+            //string mystring2 = "Elkholy hossam";
+            //string mystring3 = mystring1 + mystring2;
+         //   Console.WriteLine($"{mystring}");
+            //** 24  -Literal Characters
+            //int myvar = -100;
+            //uint myuvar = 1000u;
+            //float myflvar = 1.5f;
+            //double nydvar = 100.5;
+            //decimal mydecdvar = 100.5m;
+            //Console.WriteLine($"{myvar},{myuvar}");
+            //Console.WriteLine($"{mydecdvar} and {nydvar}");
+            Console.ReadKey();
+        }
+    }
+}
