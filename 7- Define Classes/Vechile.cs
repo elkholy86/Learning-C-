283ca95733e60dd46ef173428e75915ae3b1e8d6
elkholy86 <elkholy@windowslive.com>
2021-01-31 07:46:04 +0300

    7- Define Classes
    7- Define Classes

---
+++ 7- Define Classes/Vechile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7__Define_Classes
+{
+//     7- Define Classes
+    class Vechile
+    {
+        string manufacturere;
+        string model;
+        string color;
+        int enginesize;
+        public void painting()
+        { }public void moving()
+
+        { }
+    }
+}
