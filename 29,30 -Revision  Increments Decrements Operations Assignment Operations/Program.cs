79a8e5e07d335116325fb134afa97b19104cc116
elkholy86 <elkholy@windowslive.com>
2021-02-01 08:48:26 +0300

    29,30 - Increments - Decrements Operations & Assignment Operations
    29,30 - Increments - Decrements Operations & Assignment Operations

---
+++ 29,30 -Revision  Increments Decrements Operations Assignment Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _29_30__Revision__Increments_Decrements_Operations_Assignment_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int myvar1 = 5;
+            //int temp1 = ++myvar1 ;
+            //Console.WriteLine($"{temp1}");
+            // int myvar1 = 7, myvar2 = 3;
+            //  myvar2 = myvar1;
+            //myvar1 += myvar2;
+            // myvar2 += myvar1;
+            // myvar1 -= myvar2;
+            //   myvar1 %= myvar2;
+            int temp, myvar1 = 7, myvar2 = 2;
+            myvar1 /= ++myvar2;
+            Console.WriteLine($"The result is {myvar1} and the value of var2 = {myvar2}");
+        }
+    }
+}
