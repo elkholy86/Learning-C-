749cd9f6b226399eefe1c5c775e0a98b73c2a858
elkholy86 <elkholy@windowslive.com>
2021-02-08 08:03:20 +0300

    32 Revision on Boolean Operatorors
    32 Revision on Boolean Operatorors

---
+++ 32 Revision on 32- Boolean Operatorors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _32_Revision_on_32__Boolean_Operatorors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool myresult;
+            //int myvar1 = 5, myvar2 = 8;
+            ////myresult = myvar1 == 6;
+            ////myresult = myvar1 != 5;
+            //myresult = myvar1 != 8;
+            //myresult = (myvar1 <= 5) && (myvar2>8);
+            //Console.WriteLine($"Myresult is = {myresult}");
+            int myvar1 = 9 , myvar2 = 11 ;
+            bool myresult , myresult1;
+            myresult = (myvar1 < 10) && (myvar2 <= 11);
+            myresult1 = (myvar1 < 10) && (myvar2 <= 11);
+            Console.WriteLine($"Myresult is = {myresult}");
+            Console.WriteLine($"Myresult is = {myresult1}");
+            Console.ReadKey();
+        }
+    }
+}
