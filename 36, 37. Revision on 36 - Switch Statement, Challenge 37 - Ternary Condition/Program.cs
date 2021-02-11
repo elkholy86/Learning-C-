b21c43319d4ec681343657160380c382acc668f4
elkholy86 <elkholy@windowslive.com>
2021-02-11 08:04:23 +0300

    36, 37. Revision on (36 ) Switch Statement, Challenge (37) Ternary Condition
    36, 37. Revision on (36 ) Switch Statement, Challenge (37) Ternary Condition

---
+++ 36, 37. Revision on 36 - Switch Statement, Challenge 37 - Ternary Condition/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _36_._Revision_on_36___Switch_Statement__Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "nazar", result;
+            result = (myname.Equals("Nazar")) ? "Yes" : "No";
+            Console.WriteLine($"{result}");
+
+            //int myvar = 9;
+            //string myresult = (myvar < 8) ? "Less than 8" : "Greater than 8";
+            //Console.WriteLine($"{myresult}");
+
+            //string whathesay = "Hello";
+            //switch (whathesay)
+            //{
+            //    case "hello":
+            //        Console.WriteLine("Hello man");
+            //        break;
+            //    case "bye":
+            //        Console.WriteLine("My animal is a cat");
+            //        break;
+            //    case "Welcome":
+            //        Console.WriteLine("My animal is a camel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+
+
+            //int myvar = 15;
+            //switch (myvar)
+            //{
+            //    case 10 :
+            //        Console.WriteLine("My variable number value is 10");
+            //        break;
+            //    case 7 :
+            //        Console.WriteLine("My variable number value is 7");
+            //        break;
+            //    case 15:
+            //        Console.WriteLine("My variable number value is 15");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+
+            //}
+
+            //string myanimal = "Cat";
+            //switch (myanimal)
+            //{
+            //    case "canary":
+            //        Console.WriteLine("My animal is the canary bird");
+            //        break;
+            //    case "Cat":
+            //        Console.WriteLine("My animal is a cat");
+            //        break;
+            //    case "camel":
+            //        Console.WriteLine("My animal is a camel");
+            //        break;
+            //                        default:
+            //        Console.WriteLine("No one");
+            //        break;
+
+            //}
+        }
+    }
+}
