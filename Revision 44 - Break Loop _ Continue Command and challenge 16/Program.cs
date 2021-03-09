f35c5f288afc4b499226d316cca9f65eef22eb61
elkholy86 <elkholy@windowslive.com>
2021-03-09 07:25:16 +0300

    43 - Break Loop_Break Command and challenge 15
    43 - Revision on Break Loop_Break Command and challenge 15

---
+++ Revision 44 - Break Loop _ Continue Command and challenge 16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Revision_44___Break_Loop___Continue_Command_and_challenge_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 7)
+                        continue;
+                    for (int j= 0; j<= 10; j++)
+                {
+                    int z = i * j;
+                    Console.WriteLine($"{i} * {j} = {z}") ;
+                }
+            }
+
+
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello maxpaye");
+            //    if (i > 2)
+            //        continue;
+            //    for (int j= 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("Hello world");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
